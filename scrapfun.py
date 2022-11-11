e9ce1b47cddb8831cb2ee506037dcc1bc984189f
addienaon <adelinenaon@outlook.com>
2022-11-11 13:14:07 -0600

    get iall ngredients into dataframe

--- scrapfun.py
+++ scrapfun.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 def ingredient_count(url):
     #get number of ingredients in recipee by passing recipee url.
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-    list = doc.find('ul',class_="wprm-recipe-ingredients").find_all('li', class_="wprm-recipe-ingredient")
+    list = doc.find_all('ul',class_="wprm-recipe-ingredients")
 
     count = 0
-    for li in list:
-        count += 1
+    for ul in list:
+        for li in ul:
+            count += 1
     return(count)
 
 def get_ingredients(url, count):
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    i=1
+    df = pd.DataFrame(columns=['amount', 'unit', 'ingredient'])
     for i in range(count):
         list = doc.find_all('li', class_="wprm-recipe-ingredient")[i]
         if list.find(class_="wprm-recipe-ingredient-amount"):
             amount = list.find(class_="wprm-recipe-ingredient-amount").text
         else: amount=''
-        unit = list.find(class_="wprm-recipe-ingredient-unit").text
+        if list.find(class_="wprm-recipe-ingredient-unit"):
+            unit = list.find(class_="wprm-recipe-ingredient-unit").text
+        else: unit=''
         ingredient = list.find(class_="wprm-recipe-ingredient-name").text
-        print(amount, unit, ingredient)
+        new_row = pd.Series({'amount':amount, 'unit':unit, 'ingredient':ingredient})
+        df = pd.concat([df, new_row.to_frame().T], axis=0, ignore_index=True)
+    return(df)

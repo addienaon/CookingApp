1c1b37cab61e04acecc8d2a8d2243dfb06fd87e3
addienaon <adelinenaon@outlook.com>
2022-11-12 15:54:47 -0600

    url_container_pack and functions

--- scrape.py
+++ scrape.py
@@ -1,17 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-from scrapfun import ingredient_count, get_ingredients, get_urlpack
+from scrapfun import get_ingredients, get_urlpack
 import pandas as pd
 
-# url = "https://www.cookwithmanali.com/recipes/?_paged=1"
-
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text, "html.parser")
-
-pack = get_urlpack()
-
-for u in len(range(pack)):
-    count = ingredient_count(u)
-    ingredients = get_ingredients(u, count)
-    print(u)
-    
+# for i in range(len(url_list)):
+#     print(url_list[i])
+#     print(get_ingredients(url_list[i]))
--- scrapfun.py
+++ scrapfun.py
@@ -2,35 +2,28 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-def ingredient_count(url):
-    #get number of ingredients in recipee by passing recipee url.
+def get_ingredients(url):
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-    list = doc.find_all('ul',class_="wprm-recipe-ingredients")
-
-    count = 0
-    for ul in list:
-        for li in ul:
-            count += 1
-    return(count)
-
-def get_ingredients(url, count):
-    result = requests.get(url)
-    doc = BeautifulSoup(result.text, "html.parser")
-
-    df = pd.DataFrame(columns=['amount', 'unit', 'ingredient'])
     if doc.find(class_="wprm-recipe-container"):
-        for i in range(count):
-            list = doc.find_all('li', class_="wprm-recipe-ingredient")[i]
-            if list.find(class_="wprm-recipe-ingredient-amount"):
-                amount = list.find(class_="wprm-recipe-ingredient-amount").text
-            else: amount=''
-            if list.find(class_="wprm-recipe-ingredient-unit"):
-                unit = list.find(class_="wprm-recipe-ingredient-unit").text
-            else: unit=''
-            ingredient = list.find(class_="wprm-recipe-ingredient-name").text
-            new_row = pd.Series({'amount':amount, 'unit':unit, 'ingredient':ingredient})
-            df = pd.concat([df, new_row.to_frame().T], axis=0, ignore_index=True)
+        list = doc.find_all('ul',class_="wprm-recipe-ingredients")
+        df = pd.DataFrame(columns=['amount', 'unit', 'ingredient'])
+        count = 0
+        for ul in list:
+            for li in ul:
+                count += 1
+    
+            for i in range(count):
+                list = doc.find_all('li', class_="wprm-recipe-ingredient")[i]
+                if list.find(class_="wprm-recipe-ingredient-amount"):
+                    amount = list.find(class_="wprm-recipe-ingredient-amount").text
+                else: amount=''
+                if list.find(class_="wprm-recipe-ingredient-unit"):
+                    unit = list.find(class_="wprm-recipe-ingredient-unit").text
+                else: unit=''
+                ingredient = list.find(class_="wprm-recipe-ingredient-name").text
+                new_row = pd.Series({'amount':amount, 'unit':unit, 'ingredient':ingredient})
+                df = pd.concat([df, new_row.to_frame().T], axis=0, ignore_index=True)
     else: print('RECIPEE CONTAINER DNE')
     return(df)
 
@@ -61,3 +54,16 @@ def get_urlpack():
         doc = BeautifulSoup(result.text, "html.parser")
     return(pack)
 
+def get_container_pack():
+    pack = get_urlpack()
+    container_pack = []
+    for i in range(len(pack)):
+        result = requests.get(pack[i])
+        doc = BeautifulSoup(result.text, "html.parser")
+        if doc.find(class_="wprm-recipe-container"): 
+            container_pack.append(pack[i])
+
+    with open('url_container_pack.txt', 'w') as f:
+        for line in container_pack:
+            f.write(line)
+            f.write('\n')

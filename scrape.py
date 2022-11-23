d7367e7532780b25865c3a3be9b521e8946977f9
addienaon <adelinenaon@outlook.com>
2022-11-22 19:21:49 -0600

    Run etl file to automatically check if there are any new recipes not yet in the db, and scrape them if there are.

--- scrape.py
+++ scrape.py
@@ -1,14 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-from scrapfun import get_ingredients, get_urlpack, get_html
+from scrapfun import get_ingredients, get_urlpack, get_html, get_container_pack
 import pandas as pd
 
-# for i in range(len(url_list)):
-#     print(url_list[i])
-#     print(get_ingredients(url_list[i]))
 
-url='https://www.cookwithmanali.com/spinach-dal/#wprm-recipe-container-35643'
-
-html = get_html(url)
-#print(get_ingredients(html))
-print(html)
+        

b7030e88e3b3fabc3a83c94e54232505f2f0163f
addienaon <adelinenaon@outlook.com>
2023-01-16 18:02:48 -0600

    Html queried from database and parsed. Inserted recipe_id, ingredient_id, unit_id, and amounts into recipes table.

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, insert, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, Float
 from sqlalchemy.dialects.mysql import VARCHAR
 
 Base = declarative_base()
@@ -34,9 +34,31 @@ class Unit(Base):
     _id = Column("id", Integer, primary_key=True)
     type = Column(String(50).with_variant(VARCHAR(50, charset="utf8"), "mysql"))
 
+    def __init__(self, type):
+        self.type = type
+
 class Recipes(Base):
     __tablename__= "recipes"
-    recipe_id = Column(Integer, primary_key=True)
-    ingredient_id = Column(Integer, ForeignKey('ingrdient.id'))
-    amount = Column(Integer)
-    unit_id = Column(Integer, ForeignKey('unit.id')) 
+    _id = Column("id", Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipe.id'))
+    amount = Column(Float)
+    unit_id = Column(Integer, ForeignKey('unit.id'))
+    ingredient_id = Column(Integer, ForeignKey('ingredient.id')) 
+
+    def __init__(self, recipe_id, amount, unit_id, ingredient_id):
+        self.recipe_id = recipe_id
+        self.amount = amount
+        self.unit_id = unit_id
+        self.ingredient_id = ingredient_id
+
+class Food(Base):
+    __tablename__= "food"
+    _id = Column("id", Integer, primary_key=True)
+    name = Column(String(255).with_variant(VARCHAR(255, charset="utf8"), "mysql"))
+    name_scientific = Column(String(255).with_variant(VARCHAR(255, charset="utf8"), "mysql"))
+    description = Column(String)
+
+    def __init__(self, name, name_scientific, description):
+        self.name = name
+        self.name_scientific = name_scientific
+        self.description = description
